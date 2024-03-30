1e598b735dc5eae960200d354b0ded5eef554323
husseymarcos <mhussey@mail.austral.edu.ar>
2024-03-30 19:20:08 -0300

    fixed dependencies and tested out Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,38 @@
+import com.google.gson.Gson;
+import repository.Users;
+import spark.Spark;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 
 
 public class Application {
+    private static final Gson gson = new Gson();
+    public static void main(String[] args) {
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("lab1");
+
+        Spark.port(4321);
+
+        storedBasicUser(entityManagerFactory);
+
+        /* 1. Basic Request */
+        Spark.get("/hello",
+                (req, resp) -> "Hello, World"
+        );
+
+    }
+
+    private static void storedBasicUser(EntityManagerFactory entityManagerFactory) {
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        final Users users = new Users(entityManager);
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        tx.commit();
+        entityManager.close();
+    }
 }
