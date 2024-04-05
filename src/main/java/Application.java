3324ba1640b3a806f844316f27e7922176727b7d
husseymarcos <mhussey@mail.austral.edu.ar>
2024-04-05 10:10:02 -0300

    Spark -> SpringBoot

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,87 +1,71 @@
-import com.google.common.base.Strings;
-import com.google.gson.Gson;
+
 import model.User;
 import model.player.PlayerLoginInformation;
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import repository.Users;
-import spark.Spark;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 
+@RestController
 public class Application {
-    private static final Gson gson = new Gson();
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    public Application(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
     public static void main(String[] args) {
-        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("lab1");
-
-        Spark.port(4321);
-
-        storedBasicUser(entityManagerFactory);
-
-        /* 1. Basic Request */
-        Spark.get("/hello",
-                (req, resp) -> "Hello, World"
-        );
-
-        Spark.get("/persisted-users/:id",
-                (req, resp) -> {
-                    final String id = req.params("id");
-
-                    /* Business Logic */
-                    final EntityManager entityManager = entityManagerFactory.createEntityManager();
-                    final EntityTransaction tx = entityManager.getTransaction();
-                    tx.begin();
-                    User user = entityManager.find(User.class, Long.valueOf(id));
-                    tx.commit();
-                    entityManager.close();
-
-                    resp.type("application/json");
-                    return user.asJson();
-                }
-        );
-
-        Spark.get("/users/:name",
-                (req, resp) -> {
-                    final String name = capitalized(req.params("name"));
-
-                    final User user = User.create(name + "@gmail.com").build();
-
-                    resp.type("application/json");
-
-                    return user.asJson();
-                }
-        );
-
-        /* 7. Get all registered emails */
-        Spark.get("/emails", (req, resp) -> {
-            final EntityManager entityManager = entityManagerFactory.createEntityManager();
-            final Users users = new Users(entityManager);
-
-            List<String> emails = users.listAll().stream()
-                    .map(PlayerLoginInformation::getPlayer_email)
-                    .collect(Collectors.toList());
-
-            resp.type("application/json");
-            return gson.toJson(emails);
-        });
-
+        SpringApplication.run(Application.class, args);
     }
 
-    private static void storedBasicUser(EntityManagerFactory entityManagerFactory) {
-        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World";
+    }
 
-        EntityTransaction tx = entityManager.getTransaction();
-        tx.begin();
-
-        tx.commit();
+    @GetMapping("/persisted-users/{id}")
+    public User getUserById(@PathVariable Long id) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        User user = entityManager.find(User.class, id);
         entityManager.close();
+        return user;
     }
 
-    private static String capitalized(String name) {
-        return Strings.isNullOrEmpty(name) ? name : name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    @GetMapping("/users/{name}")
+    public User getUserByName(@PathVariable String name) {
+        String formattedName = capitalize(name);
+        return User.create(formattedName + "@gmail.com").build();
+    }
+
+    @GetMapping(value = "/emails", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getAllEmails() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Users usersRepository = new Users(entityManager);
+
+        List<String> emails = usersRepository.listAll().stream()
+                .map(PlayerLoginInformation::getPlayer_email)
+                .collect(Collectors.toList());
+
+        entityManager.close();
+        return emails;
+    }
+
+    private String capitalize(String name) {
+        if (name == null || name.isEmpty()) {
+            return name;
+        }
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
     }
 }
