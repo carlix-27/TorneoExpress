01df887fa2fe7489efba853cef809c9ff4e9cbec
husseymarcos <mhussey@mail.austral.edu.ar>
2024-04-09 09:52:42 -0300

    Update TorneosExpress.java

--- src/main/java/com/TorneosExpress/TorneosExpress.java
+++ src/main/java/com/TorneosExpress/TorneosExpress.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class TorneosExpress {
 
+    //hello
     public static void main(String[] args) {
         SpringApplication.run(TorneosExpress.class, args);
     }
