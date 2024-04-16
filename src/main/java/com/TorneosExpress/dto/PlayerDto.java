f3409179cff1ed591b9b3b350601c07cfd06cc53
husseymarcos <mhussey@mail.austral.edu.ar>
2024-04-16 10:45:58 -0300

    Create PlayerDto.java

---
+++ src/main/java/com/TorneosExpress/dto/PlayerDto.java
@@ -0,0 +1,103 @@
+package com.TorneosExpress.dto;
+
+import com.TorneosExpress.model.Team;
+
+import java.util.List;
+
+public class PlayerDto {
+    private Long id;
+    private String name;
+    private String location;
+    private String email;
+    private Boolean isPremium;
+    private Boolean isEnabled;
+    private String password;
+    private List<Team> ownedTeams;
+    private boolean isCaptain;
+
+    public PlayerDto(Long id, String name, String location, String email, Boolean isPremium, Boolean isEnabled, String password, List<Team> ownedTeams, boolean isCaptain) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.email = email;
+        this.isPremium = isPremium;
+        this.isEnabled = isEnabled;
+        this.password = password;
+        this.ownedTeams = ownedTeams;
+        this.isCaptain = isCaptain;
+    }
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getPremium() {
+        return isPremium;
+    }
+
+    public void setPremium(Boolean premium) {
+        isPremium = premium;
+    }
+
+    public Boolean getEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Team> getOwnedTeams() {
+        return ownedTeams;
+    }
+
+    public void setOwnedTeams(List<Team> ownedTeams) {
+        this.ownedTeams = ownedTeams;
+    }
+
+    public boolean isCaptain() {
+        return isCaptain;
+    }
+
+    public void setCaptain(boolean captain) {
+        isCaptain = captain;
+    }
+}
