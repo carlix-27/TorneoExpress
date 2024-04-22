fcf3f7a8da0262d1fa03af72456a93e69446a88e
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-04-22 09:41:02 -0300

    request

--- src/main/java/com/TorneosExpress/dto/CreateTeamRequest.java
+++ src/main/java/com/TorneosExpress/dto/CreateTeamRequest.java
@@ -1,4 +1,32 @@
 package com.TorneosExpress.dto;
 
+import com.TorneosExpress.model.Sport;
+import com.TorneosExpress.model.player.Player;
+
 public class CreateTeamRequest {
+  private String teamName;
+  private String location;
+  private String sport;
+  private boolean isPrivate;
+  private Player captain;
+
+  public Player getCaptain() {
+    return captain;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public String getSport() {
+    return sport;
+  }
+
+  public String getTeamName() {
+    return teamName;
+  }
+
+  public boolean isPrivate() {
+    return isPrivate;
+  }
 }
