0678008710210eef4335c2ec72eb848c0168ec76
husseymarcos <mhussey@mail.austral.edu.ar>
2024-04-21 22:06:46 -0300

    added logout

--- src/main/resources/static/js/logout.js
+++ src/main/resources/static/js/logout.js
@@ -1,15 +1,4 @@
 function logout() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/api/auth/logout', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            localStorage.removeItem("userID"); // Clear userID from localStorage
-            window.location.replace("login.html"); // Redirect to login page after logout
-        } else {
-            console.error(xhr.responseText);
-            // Handle any error response
-        }
-    };
-    xhr.send();
+    localStorage.removeItem("userId"); // Remove userId from localStorage
+    window.location.href = "login.html"; // Redirect to login page after logout
 }
