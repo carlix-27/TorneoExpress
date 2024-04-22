a3b188b0b2b6c126c6179591cc4d6a6174359a7a
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-04-22 09:39:27 -0300

    dto created

--- src/main/java/com/TorneosExpress/dto/TeamDto.java
+++ src/main/java/com/TorneosExpress/dto/TeamDto.java
@@ -1,5 +1,80 @@
 package com.TorneosExpress.dto;
 
-public class TeamDto {
+import com.TorneosExpress.model.Sport;
+import com.TorneosExpress.model.Tournament;
+import com.TorneosExpress.model.player.Player;
+import com.TorneosExpress.model.shop.Article;
 
+import java.util.List;
+
+public class TeamDto {
+  private Long id;
+  private String teamName;
+  private String location;
+  private String sport;
+  private boolean isPrivate;
+  private int prestigePoints;
+  private Player captain;
+  private List<Player> players;
+  private List<Tournament> tournaments;
+  private List<Article> articles;
+  private List<Player> joinRequests;
+
+  public TeamDto(Long id, String teamName, String location, String sport, boolean isPrivate, int prestigePoints, Player captain, List<Player> players, List<Article> articles, List<Player> joinRequests, List<Tournament> tournaments) {
+    this.id = id;
+    this.teamName = teamName;
+    this.location = location;
+    this.sport = sport;
+    this.isPrivate = isPrivate;
+    this.prestigePoints = prestigePoints;
+    this.captain = captain;
+    this.players = players;
+    this.tournaments = tournaments;
+    this.articles = articles;
+    this.joinRequests = joinRequests;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTeamName() {
+    return teamName;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public String getSport() {
+    return sport;
+  }
+
+  public boolean isPrivate() {
+    return isPrivate;
+  }
+
+  public int getPrestigePoints() {
+    return prestigePoints;
+  }
+
+  public Player getCaptain() {
+    return captain;
+  }
+
+  public List<Article> getArticles() {
+    return articles;
+  }
+
+  public List<Player> getJoinRequests() {
+    return joinRequests;
+  }
+
+  public List<Player> getPlayers() {
+    return players;
+  }
+
+  public List<Tournament> getTournaments() {
+    return tournaments;
+  }
 }
