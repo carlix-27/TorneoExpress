01005c93f7ed7cbf92f4c9703081f719a8d006d6
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-05-28 11:49:49 -0300

    additional implementation to test

--- src/main/java/com/TorneosExpress/fixture/FixtureBuilder.java
+++ src/main/java/com/TorneosExpress/fixture/FixtureBuilder.java
@@ -19,33 +19,65 @@ public class FixtureBuilder {
   }
 
   public Fixture build(List<Team> teams) {
-    return new Fixture(calculateMatchCalendar(teams));
+    //return new Fixture(calculateMatchCalendar(teams));
+    return new Fixture(otherImplementation(teams));
+  }
+
+  private List<Match> otherImplementation(List<Team> teams) {
+    List<Match> matches = new ArrayList<>();
+    int numWeeks = teams.size() - 1; // Teams play against everyone except for themselves.
+
+    for (int week = 0; week < numWeeks; week++) {
+      List<Long> hasMatchForWeek = new ArrayList<>();
+      processTeams(matches, teams, hasMatchForWeek, week);
+    }
+    return matches;
+  }
+
+  private void processTeams(List<Match> matches, List<Team> teams, List<Long> hasMatchForWeek, int week) {
+    for (int i = 0; i < teams.size() - 1; i++) {
+      if (isFreeOnWeek(teams.get(i).getId(), teams.get(i + 1).getId(), hasMatchForWeek)) {
+        matches.add(new Match(
+            teams.get(i).getId(),
+            teams.get(i + 1).getId(),
+            tournamentId,
+            location,
+            startDate.plusWeeks(week),
+            "To be played"
+        ));
+        hasMatchForWeek.add(teams.get(i).getId());
+        hasMatchForWeek.add(teams.get(i + 1).getId());
+      }
+    }
   }
 
   private List<Match> calculateMatchCalendar(List<Team> teams) {
     List<Match> matches = new ArrayList<>();
     Collections.shuffle(teams);
     /* Shuffle so that there are less misses. */
-    for (int i = 0; i < teams.size() - 1; i++) {
+    for (int numTeams = 0; numTeams < teams.size() - 1; numTeams++) {
       List<Long> hasMatchOnWeek = new ArrayList<>();
-      processWeek(teams, i, hasMatchOnWeek, matches);
+      processWeek(teams, numTeams, hasMatchOnWeek, matches);
     }
     return matches;
   }
 
-  private void processWeek(List<Team> teams, int i, List<Long> hasMatchOnWeek, List<Match> matches) {
-    for (int j = i + 1; j < teams.size(); j++) {
-      if (isFreeOnWeek(teams.get(i).getId(), teams.get(j).getId(), hasMatchOnWeek)) {
+  private void processWeek(List<Team> teams, int numTeams, List<Long> hasMatchOnWeek, List<Match> matches) {
+    for (int numWeeks = numTeams + 1; numWeeks < teams.size(); numWeeks++) {
+      if (isFreeOnWeek(teams.get(numTeams).getId(), teams.get(numWeeks).getId(), hasMatchOnWeek)) {
         matches.add(new Match(
-            teams.get(i).getId(),
-            teams.get(j).getId(),
+            teams.get(numTeams).getId(),
+            teams.get(numWeeks).getId(),
             tournamentId,
             location,
-            startDate.plusWeeks(j),
+            startDate.plusWeeks(numWeeks),
             "To be played"
         ));
-        hasMatchOnWeek.add(teams.get(i).getId());
-        hasMatchOnWeek.add(teams.get(j).getId());
+        hasMatchOnWeek.add(teams.get(numTeams).getId());
+        hasMatchOnWeek.add(teams.get(numWeeks).getId());
+        if (hasMatchOnWeek.size() >= numTeams) {
+          break; // Limit matches per week
+        }
       }
     }
   }
