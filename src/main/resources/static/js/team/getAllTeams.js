4976184a67656f8b6baadb9b1617d04428cdf8bf
husseymarcos <mhussey@mail.austral.edu.ar>
2024-06-04 11:02:24 -0300

    fixed getAllTeams

--- src/main/resources/static/js/team/getAllTeams.js
+++ src/main/resources/static/js/team/getAllTeams.js
@@ -1,4 +1,43 @@
-import { loadTeams } from '/loadTeams.js';
+function loadTeams() {
+    fetch(`/api/teams/all`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch teams: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(teams => {
+            const listaEquipos = document.getElementById("lista-todos-equipos");
+
+            listaEquipos.innerHTML = '';
+
+            teams.forEach(team => {
+
+                const li = document.createElement("li");
+                li.innerHTML = `
+                    <div>
+                        <a href="loadTeam.html?id=${team.id}"><h3>${team.name}</h3></a>
+                        <p>Ubicación: ${team.location}</p>
+                        <p>Deporte: ${team.sport.sportName}</p>
+                        <p>Privacidad: ${team.isPrivate ? "Privado" : "Público"}</p>
+                        <p>Jugadores inscritos: ${team.players.length} / ${team.sport.num_players * 2}</p>
+                        <button class="signup-button" data-team-id="${team.id}">Signup</button>
+                `;
+                listaEquipos.appendChild(li);
+            });
+
+            // Attach event listeners to the signup buttons
+            document.querySelectorAll('.signup-button').forEach(button => {
+                button.addEventListener('click', function () {
+                    showSignupModal(this.getAttribute('data-team-id'));
+                });
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Handle error, show message to user
+        });
+}
 
 export function showSignupModal(teamId) {
     const modal = document.getElementById("signupModal");
@@ -74,12 +113,6 @@ function displayModal(modal, closeButton) {
             modal.style.display = "none";
         }
     };
-}
-
-function displaySuccessMessage(message) {
-    const successMessage = document.getElementById("successMessage");
-    successMessage.textContent = message;
-    successMessage.style.display = "block";
 }
 
 document.addEventListener("DOMContentLoaded", loadTeams);
