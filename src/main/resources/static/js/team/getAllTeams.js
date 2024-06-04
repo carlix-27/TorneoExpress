db6222a551abff97e78c018e518d4b556f229377
husseymarcos <mhussey@mail.austral.edu.ar>
2024-06-04 06:27:47 -0300

    sendInvite.js

--- src/main/resources/static/js/team/getAllTeams.js
+++ src/main/resources/static/js/team/getAllTeams.js
@@ -1,7 +1,5 @@
-// Load teams
 loadTeams()
 
-// Show signup modal
 function showSignupModal(teamId) {
     const modal = document.getElementById("signupModal");
     const closeButton = modal.querySelector(".close");
@@ -57,41 +55,11 @@ function addSignupButtonListener(team, userId, signupButton) {
                 sendInvite(team, userId);
             } else {
                 console.log("Player is already part of the team.");
-                // Handle case where player is already part of the team
             }
         } else {
             console.log("Maximum number of players reached.");
-            // Handle case where maximum number of players is reached
         }
     });
-}
-
-function sendInvite(team, userId) {
-    fetch(`/api/invites/send`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            inviterId: userId,
-            inviteeId: team.captain_id,
-            teamId: team.id
-        })
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to send invite: ${response.status} ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(invite => {
-            console.log("Invite sent successfully.");
-            createNotification(team, userId);
-        })
-        .catch(error => {
-            console.error("Error sending invite:", error);
-            // Handle error sending invite
-        });
 }
 
 function displayModal(modal, closeButton) {
