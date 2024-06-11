8ae368e0d5eba9982bd843cb56391e18bd33e174
husseymarcos <mhussey@mail.austral.edu.ar>
2024-06-11 06:45:31 -0300

    fix tournamentRequest and notification creation

--- src/main/java/com/TorneosExpress/service/RequestService.java
+++ src/main/java/com/TorneosExpress/service/RequestService.java
@@ -14,15 +14,17 @@ public class RequestService {
     private final TeamRequestRepository teamRequestRepository;
     private final TournamentRequestRepository tournamentRequestRepository;
     private final TeamRepository teamRepository;
+    private final TournamentRepository tournamentRepository;
     private final PlayerRepository playerRepository;
 
     @Autowired
-    public RequestService(InviteRepository inviteRepository, TeamRequestRepository teamRequestRepository, TeamRepository teamRepository, PlayerRepository playerRepository, TournamentRequestRepository tournamentRequestRepository) {
+    public RequestService(InviteRepository inviteRepository, TeamRequestRepository teamRequestRepository, TeamRepository teamRepository, PlayerRepository playerRepository, TournamentRequestRepository tournamentRequestRepository, TournamentRepository tournamentRepository) {
         this.inviteRepository = inviteRepository;
         this.teamRequestRepository = teamRequestRepository;
         this.teamRepository = teamRepository;
         this.playerRepository = playerRepository;
         this.tournamentRequestRepository = tournamentRequestRepository;
+        this.tournamentRepository = tournamentRepository;
     }
 
     public Invite sendInvite(Long from, Long to, Long team) {
@@ -56,13 +58,6 @@ public class RequestService {
         return tournamentRequestRepository.save(request);
     }
 
-    public List<TeamRequest> getAllTeamRequestsByToId(Long toId) {
-        return teamRequestRepository.findByrequestTo(toId);
-    }
-
-    public List<TournamentRequest> getAllTournamentRequestsByToId(Long toId) {
-        return tournamentRequestRepository.findByrequestTo(toId);
-    }
 
     public List<TeamRequest> getRequestsByTeam(Long toId, Long teamId) {
         return teamRequestRepository.findByRequestToAndTeamId(toId, teamId);
@@ -95,6 +90,32 @@ public class RequestService {
         return request;
     }
 
+
+    public TournamentRequest acceptTournamentRequest(Long requestId) {
+        TournamentRequest request = tournamentRequestRepository.findById(requestId)
+                .orElseThrow(() -> new RuntimeException("Team request not found"));
+
+        if (request.isAccepted() || request.isDenied()) {
+            throw new RuntimeException("Team request already processed");
+        }
+
+        request.setAccepted(true);
+        Tournament tournament = tournamentRepository.findById(request.getTeamId())
+                .orElseThrow(() -> new RuntimeException("Team not found"));
+
+        Team team = teamRepository.findById(request.getRequestFrom())
+                .orElseThrow(() -> new RuntimeException("Player not found"));
+
+        tournament.getParticipatingTeams().add(team);
+
+
+        tournamentRepository.save(tournament);
+
+        tournamentRequestRepository.delete(request);
+
+        return request;
+    }
+
     public TeamRequest denyTeamRequest(Long requestId) {
         TeamRequest request = teamRequestRepository.findById(requestId)
                 .orElseThrow(() -> new RuntimeException("Team request not found"));
@@ -105,6 +126,21 @@ public class RequestService {
 
         request.setDenied(true);
         teamRequestRepository.delete(request);
+
+        return request;
+    }
+
+
+    public TournamentRequest denyTournamentRequest(Long requestId) {
+        TournamentRequest request = tournamentRequestRepository.findById(requestId)
+                .orElseThrow(() -> new RuntimeException("Team request not found"));
+
+        if (request.isAccepted() || request.isDenied()) {
+            throw new RuntimeException("Team request already processed");
+        }
+
+        request.setDenied(true);
+        tournamentRequestRepository.delete(request);
 
         return request;
     }
---
+++ src/main/resources/static/js/tournament/manageTournamentRequests.js
@@ -0,0 +1,100 @@
+function loadTournamentRequests(tournamentId) {
+    const userId = localStorage.getItem("userId");
+    if (!userId) {
+        console.error("User ID not found in localStorage");
+        return;
+    }
+
+    // Solo fetch teamReque
+    fetchTournamentRequests(userId, tournamentId)
+}
+
+
+
+function fetchTournamentRequests(userId, tournamentId){
+    return fetch(`/api/requests/tournament/${userId}/${teamId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch team requests: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(requests => {
+            const requestsContainer = document.getElementById("team-requests");
+            requestsContainer.innerHTML = '';
+
+            requests.forEach(request => {
+                const requestElement = document.createElement('div');
+                const playerRequesting = request.name;
+
+                requestElement.className = 'request';
+                requestElement.innerHTML = `
+                            <p>From: ${playerRequesting}</p>
+                            <div class="button-container">
+                                <button class="manage-button accept-button" data-request-id="${request.id}">Accept</button>
+                                <button class="manage-button deny-button" data-request-id="${request.id}">Deny</button>
+                            </div>
+                        `;
+                requestsContainer.appendChild(requestElement);
+            });
+
+            document.querySelectorAll('.accept-button').forEach(button => {
+                button.addEventListener('click', handleAccept);
+            });
+
+            document.querySelectorAll('.deny-button').forEach(button => {
+                button.addEventListener('click', handleDeny);
+            });
+        });
+}
+
+function handleAccept(event) {
+    const requestId = event.target.getAttribute('data-request-id');
+    updateRequestStatus(requestId, true);
+}
+
+function handleDeny(event) {
+    const requestId = event.target.getAttribute('data-request-id');
+    updateRequestStatus(requestId, false);
+}
+
+
+function updateRequestStatus(requestId, accepted) {
+    const acceptUrl = `/api/requests/tournament/${requestId}/accept`;
+    const denyUrl = `/api/requests/tournament/${requestId}/deny`;
+
+    const url = accepted ? acceptUrl : denyUrl;
+
+    fetch(url, {
+        method: 'DELETE',
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to update request: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(() => {
+            loadTournamentRequests();
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+
+function getTournamentFromUrl() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('id');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const tournamentId = getTeamIdFromUrl();
+    if (tournamentId) {
+        loadTeamRequests(tournamentId);
+    } else {
+        console.error('Team ID not found in URL');
+    }
+});
+
+
