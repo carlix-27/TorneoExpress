d22e0d046620f2883b42088dec63dbf15710f5e0
husseymarcos <mhussey@mail.austral.edu.ar>
2024-06-22 18:24:20 -0300

    fix html css agregar deporte

--- src/main/java/com/TorneosExpress/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.TorneosExpress.repository;
-
-import com.TorneosExpress.model.admin.AdminLoginInformation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<AdminLoginInformation, Long> {
-}
