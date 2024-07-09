4b905365761201bf6fe5ea91450942f0bbc60f6c
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-07-09 19:04:48 -0300

    service, controller & repository for Article entity

---
+++ src/main/java/com/TorneosExpress/controller/ArticleController.java
@@ -0,0 +1,18 @@
+package com.TorneosExpress.controller;
+
+import com.TorneosExpress.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/articles")
+public class ArticleController {
+  private final ArticleService articleService;
+
+  @Autowired
+  public ArticleController(ArticleService articleService) {
+    this.articleService = articleService;
+  }
+
+}
---
+++ src/main/java/com/TorneosExpress/repository/ArticleRepository.java
@@ -0,0 +1,8 @@
+package com.TorneosExpress.repository;
+
+import com.TorneosExpress.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
