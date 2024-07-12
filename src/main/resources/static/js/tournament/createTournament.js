2ee0edf59a2e4c44a2b2cbcfbdb7cbfb233ba21b
husseymarcos <mhussey@mail.austral.edu.ar>
2024-07-12 13:15:14 -0300

    crea torneo con autocomplete (sin mapa)

--- src/main/resources/static/js/tournament/createTournament.js
+++ src/main/resources/static/js/tournament/createTournament.js
@@ -1,5 +1,23 @@
+let autocomplete;
+
+function initAutocomplete() {
+    const input = document.getElementById('location');
+    autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.addListener('place_changed', function() {
+        const place = autocomplete.getPlace();
+        if (place.geometry) {
+            const location = place.geometry.location;
+            document.getElementById('location').dataset.latitude = location.lat();
+            document.getElementById('location').dataset.longitude = location.lng();
+        } else {
+            console.error('No details available for input: ' + place.name);
+        }
+    });
+}
+
+
 function fetchSports() {
-    fetch('/api/sports') // Assuming this endpoint returns the list of sports
+    fetch('/api/sports')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Failed to fetch sports: ${response.status} ${response.statusText}`);
@@ -10,97 +28,87 @@ function fetchSports() {
             const sportDropdown = document.getElementById('sport');
             sports.forEach(sport => {
                 const option = document.createElement('option');
-                option.value = sport.sportId; // Assuming sportId is the ID field in your Sport entity
-                option.text = sport.sportName; // Assuming sportName is the name field in your Sport entity
+                option.value = sport.sportId;
+                option.text = sport.sportName;
                 sportDropdown.appendChild(option);
             });
         })
         .catch(error => {
             console.error('Error:', error);
-            // Handle error, show message to user or retry fetch
         });
 }
 
-
 function createTournament() {
     const name = document.getElementById('tournament-name').value;
-    const sportId = document.getElementById('sport').value; // Get the selected sportId
-    const location = document.getElementById('location').value;
+    const sportId = document.getElementById('sport').value;
+
+    const latitude = document.getElementById('location').dataset.latitude;
+    console.log("Latitude: ", latitude)
+
+    const longitude = document.getElementById('location').dataset.longitude;
+    console.log("Longitude: ", longitude)
+
+    const location = `${latitude},${longitude}`;
+    console.log("Location: ", location)
+
     const date = document.getElementById('start-date').value;
     const isPrivate = document.getElementById('privacy').checked;
     const difficulty = document.getElementById('difficulty').value;
     const maxTeams = document.getElementById('maxTeams').value;
-
     const userId = localStorage.getItem("userId");
 
-    // Check if tournament name is blank
     if (!name.trim()) {
-        document.getElementById('error-message').innerText = "Tournament name cannot be blank.";
-        document.getElementById('error-message').style.display = 'block';
-        document.getElementById('success-message').style.display = 'none';
-        return;
+        displayErrorMessage("Tournament name cannot be blank.")
+        return
     }
 
-    // Check if user is premium
-    checkPremiumStatus(userId, function(isPremium) {
-        if (isPremium) {
-            const tournamentData = {
-                name: name,
-                sport: { sportId: sportId }, // Set the sportId in a nested object
-                location: location,
-                date: date,
-                isPrivate: isPrivate,
-                difficulty: difficulty,
-                creatorId: userId,
-                maxTeams: maxTeams,
-                isActive: true
-            };
+    const tournamentData = {
+        name: name,
+        sport: { sportId: sportId },
+        location: location,
+        date: date,
+        isPrivate: isPrivate,
+        difficulty: difficulty,
+        creatorId: userId,
+        maxTeams: maxTeams,
+        isActive: true
+    };
 
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', '/api/tournaments/create', true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    const createdTournament = JSON.parse(xhr.responseText);
-                    console.log('Tournament created:', createdTournament);
+    console.log(tournamentData)
 
-                    // Display success message in green
-                    document.getElementById('success-message').innerText = "Torneo creado con exito!";
-                    document.getElementById('success-message').style.color = 'green';
-                    document.getElementById('success-message').style.display = 'block';
-                    document.getElementById('error-message').style.display = 'none';
-                    document.getElementById('create-tournament-form').reset();
-                } else if (xhr.status === 409) {
-                    // Conflict - Tournament name must be unique
-                    document.getElementById('error-message').innerText = "El nombre del torneo debe ser ú nico, por favor elegir un nuevo nombre.";
-                    document.getElementById('error-message').style.color = 'red';
-                    document.getElementById('error-message').style.display = 'block';
-                    document.getElementById('success-message').style.display = 'none';
-                } else {
-                    console.error("Error:", xhr.status, xhr.responseText);
-                }
-            };
-            xhr.send(JSON.stringify(tournamentData));
-        } else {
-            window.location.href = "buy_premium.html"; // Redirect to buy premium page
-        }
-    });
-}
-
-
-function checkPremiumStatus(userId, callback) {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', '/api/user/' + userId + '/premium', true);
+    xhr.open('POST', '/api/tournaments/create', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = function() {
         if (xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            const isPremium = response.isPremium;
-            callback(isPremium);
+            const createdTournament = JSON.parse(xhr.responseText);
+            displaySuccessMessage("Torneo creado con exito!")
+        } else if (xhr.status === 500) {
+            displayErrorMessage("El nombre del torneo debe ser único, por favor elegir un nuevo nombre.")
         } else {
-            console.error(xhr.responseText);
-            callback(false); // Default to not premium if there's an error
+            console.error("Error:", xhr.status, xhr.responseText);
         }
     };
-    xhr.send();
+    xhr.send(JSON.stringify(tournamentData));
+
 }
+
+function displaySuccessMessage(message) {
+    const successMessage = document.getElementById("successMessage");
+    successMessage.textContent = message;
+    successMessage.style.display = "block";
+}
+
+function displayErrorMessage(message) {
+    const errorMessage = document.getElementById("errorMessage");
+    errorMessage.textContent = message;
+    errorMessage.style.display = "block";
+    setTimeout(() => {
+        errorMessage.style.display = "none";
+    }, 3000);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    initAutocomplete();
+    fetchSports();
+});
