b3832b8f39f6ad079965ed6fae8c0baecba32d5c
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2024-07-13 13:10:16 -0300

    Usuario vip verificado

---
+++ src/main/resources/static/js/user/userVip.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const userId = localStorage.getItem("userId");
+    fetchUserInfo(userId);
+});
+
+function fetchUserInfo(userId) {
+    fetch(`/api/players/${userId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch user info: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(user => {
+            console.log(user);
+            const nameElement = document.getElementById('user-name');
+            nameElement.textContent = user.name;
+
+            if (user.isPremium) {
+                document.getElementById("verified-icon").style.display = "inline";
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching user info:', error);
+        });
+}
