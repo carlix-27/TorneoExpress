1895ff1928094eb7d4a3724a16983f02e8d0a210
husseymarcos <mhussey@mail.austral.edu.ar>
2024-09-27 19:51:15 -0300

    chore: delete useless stuff

--- src/main/java/com/TorneosExpress/TorneosExpress.java
+++ src/main/java/com/TorneosExpress/TorneosExpress.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TorneosExpress {
 
-    //hello
     public static void main(String[] args) {
         SpringApplication.run(TorneosExpress.class, args);
     }
