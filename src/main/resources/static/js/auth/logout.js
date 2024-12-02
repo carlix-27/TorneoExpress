114afacb555a46ba89a8ea2cae467d90ac102042
husseymarcos <mhussey@mail.austral.edu.ar>
2024-12-02 18:13:11 -0300

    primeros cambios home

--- src/main/resources/static/js/auth/logout.js
+++ src/main/resources/static/js/auth/logout.js
@@ -2,3 +2,13 @@ function logout() {
     localStorage.removeItem("userId"); // Remove userId from localStorage
     window.location.href = "index.html"; // Redirect to login page after logout
 }
+
+document.getElementById("logout-link").addEventListener("click", confirmLogout);
+
+function confirmLogout() {
+    const logoutConfirmed = window.confirm('Do you want to logout?');
+    if (logoutConfirmed) {
+        localStorage.removeItem("userId");
+        window.location.href = "login.html";
+    }
+}
