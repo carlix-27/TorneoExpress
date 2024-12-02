ab9a93ebb4af909e5ad0fe9bdfbb8968060fad7a
husseymarcos <mhussey@mail.austral.edu.ar>
2024-12-02 18:36:55 -0300

    fix home css

--- src/main/resources/static/js/auth/logout.js
+++ src/main/resources/static/js/auth/logout.js
@@ -1,6 +1,7 @@
 function logout() {
-    localStorage.removeItem("userId"); // Remove userId from localStorage
-    window.location.href = "index.html"; // Redirect to login page after logout
+    confirmLogout()
+    localStorage.removeItem("userId");
+    window.location.href = "index.html";
 }
 
 document.getElementById("logout-link").addEventListener("click", confirmLogout);
@@ -9,6 +10,6 @@ function confirmLogout() {
     const logoutConfirmed = window.confirm('Do you want to logout?');
     if (logoutConfirmed) {
         localStorage.removeItem("userId");
-        window.location.href = "login.html";
+        window.location.href = "index.html";
     }
 }
