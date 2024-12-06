a859c8dd32761fe1455f190860a6077a5434911a
husseymarcos <mhussey@mail.austral.edu.ar>
2024-12-06 12:16:24 -0300

    fixing logout

--- src/main/resources/static/js/auth/logout.js
+++ src/main/resources/static/js/auth/logout.js
@@ -1,21 +1,17 @@
 const logoutButton = document.getElementById("logout-button");
-const logoutButtons = document.getElementById("logout-buttons");
+const logoutModal = document.getElementById("logout-modal");
 const confirmLogoutButton = document.getElementById("confirm-logout");
 const cancelLogoutButton = document.getElementById("cancel-logout");
 
-logoutButton.addEventListener("click", function() {
-    // Show the confirm/cancel buttons when clicking the logout button
-    logoutButtons.style.display = "inline-flex"; // Make the buttons appear
-    console.log("Logout buttons should now be visible.");
+logoutButton.addEventListener("click", function () {
+    logoutModal.style.display = "flex";
 });
 
-confirmLogoutButton.addEventListener("click", function() {
-    // Perform logout and redirect to the home page
+cancelLogoutButton.addEventListener("click", function () {
+    logoutModal.style.display = "none";
+});
+
+confirmLogoutButton.addEventListener("click", function () {
     localStorage.removeItem("userId");
     window.location.href = "index.html";
-});
-
-cancelLogoutButton.addEventListener("click", function() {
-    // Hide the buttons if cancel is clicked
-    logoutButtons.style.display = "none"; // Hide the buttons
 });
