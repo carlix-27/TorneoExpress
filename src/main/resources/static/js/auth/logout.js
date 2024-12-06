aa3f1cd0f87bc10c666b794517886e354f12862f
husseymarcos <mhussey@mail.austral.edu.ar>
2024-12-06 12:20:42 -0300

    logout modal appears

--- src/main/resources/static/js/auth/logout.js
+++ src/main/resources/static/js/auth/logout.js
@@ -4,14 +4,17 @@ const confirmLogoutButton = document.getElementById("confirm-logout");
 const cancelLogoutButton = document.getElementById("cancel-logout");
 
 logoutButton.addEventListener("click", function () {
+    console.log("Logout button clicked");
     logoutModal.style.display = "flex";
 });
 
 cancelLogoutButton.addEventListener("click", function () {
+    console.log("Cancel button clicked");
     logoutModal.style.display = "none";
 });
 
 confirmLogoutButton.addEventListener("click", function () {
+    console.log("Confirm logout clicked");
     localStorage.removeItem("userId");
     window.location.href = "index.html";
 });
