e795487f77280d2b3907b3d0ad3a9eaac806e112
husseymarcos <mhussey@mail.austral.edu.ar>
2024-12-09 23:28:12 -0300

    fix message css

--- src/main/resources/static/js/message/fieldMessages.js
+++ src/main/resources/static/js/message/fieldMessages.js
@@ -2,6 +2,7 @@ function displayErrorMessage(message) {
     const errorMessage = document.getElementById("errorMessage");
     errorMessage.textContent = message;
     errorMessage.style.display = "block";
+    errorMessage.className = 'message error show';
     setTimeout(() => {
         errorMessage.style.display = "none";
     }, 3000);
@@ -11,6 +12,7 @@ function displaySuccessMessage(message) {
     const successMessage = document.getElementById("successMessage");
     successMessage.textContent = message;
     successMessage.style.display = "block";
+    successMessage.className = 'message success show';
     setTimeout(() => {
         successMessage.style.display = "none";
     }, 3000);
--- src/main/resources/static/js/sport/addSport.js
+++ src/main/resources/static/js/sport/addSport.js
@@ -1,8 +1,6 @@
 function addSport() {
     const sportName = document.getElementById('sport-name').value;
     const num_players = document.getElementById('num_players').value;
-    const userId = localStorage.getItem("userId");
-
 
     if (!sportName.trim()) {
         displayErrorMessage("Nombre del deporte no puede estar vació.")
@@ -14,51 +12,29 @@ function addSport() {
         return;
     }
 
-    checkPremiumStatus(userId, function (isPremium){
-        if (isPremium) {
-            const createSportRequest = {
-                name: sportName,
-                num_players: num_players,
-            };
 
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', '/api/sports/create', true);
-            xhr.setRequestHeader('Content-Type', 'application/json');
+    const createSportRequest = {
+        name: sportName,
+        num_players: num_players,
+    };
 
-            xhr.onload = function () {
-                if (xhr.status === 200) {
-                    const response = JSON.parse(xhr.responseText);
-                    console.log('Deporte creado: ', response);
-
-                    displaySuccessMessage("Deporte creado con éxito")
-                    document.getElementById('add-sport-form').reset();
-                } else if (xhr.status === 500) {
-                    displayErrorMessage("Nombre del deporte debe ser único")
-                } else {
-                    console.error("Error:", xhr.status, xhr.responseText);
-                }
-            };
-
-            xhr.send(JSON.stringify(createSportRequest));
-        } else {
-            window.location.href = "premium.html";
-        }
-    });
-}
-
-function checkPremiumStatus(userId, callback){
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', '/api/user/' + userId + '/premium', true);
+    xhr.open('POST', '/api/sports/create', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
+
+    xhr.onload = function () {
         if (xhr.status === 200) {
             const response = JSON.parse(xhr.responseText);
-            const isPremium = response.isPremium;
-            callback(isPremium);
+            console.log('Deporte creado: ', response);
+
+            displaySuccessMessage("Deporte creado con éxito")
+            document.getElementById('add-sport-form').reset();
+        } else if (xhr.status === 500) {
+            displayErrorMessage("Nombre del deporte debe ser único")
         } else {
-            console.error(xhr.responseText);
-            callback(false);
+            console.error("Error:", xhr.status, xhr.responseText);
         }
     };
-    xhr.send();
+
+    xhr.send(JSON.stringify(createSportRequest));
 }
