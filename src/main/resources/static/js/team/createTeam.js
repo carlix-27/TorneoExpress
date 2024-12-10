619f726380df80749f5d53d2db6f8344ef0b26d0
husseymarcos <mhussey@mail.austral.edu.ar>
2024-12-10 12:31:29 -0300

    clearing code

--- src/main/resources/static/js/team/createTeam.js
+++ src/main/resources/static/js/team/createTeam.js
@@ -1,55 +1,3 @@
-let autocomplete;
-
-function initAutocomplete(apiKey) {
-    const input = document.getElementById('location');
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initializeAutocomplete`;
-    script.async = true;
-    script.defer = true;
-    document.head.appendChild(script);
-}
-
-function initializeAutocomplete() {
-    const input = document.getElementById('location');
-    if (!input) {
-        console.error("No input element with ID 'location' found.");
-        return;
-    }
-    autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.addListener('place_changed', function() {
-        const place = autocomplete.getPlace();
-        if (place.geometry) {
-            const location = place.geometry.location;
-            input.dataset.latitude = location.lat();
-            input.dataset.longitude = location.lng();
-        } else {
-            console.error('No details available for input: ' + place.name);
-        }
-    });
-}
-
-function fetchSports() {
-    fetch('/api/sports')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to fetch sports: ${response.status} ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(sports => {
-            const sportDropdown = document.getElementById('sport');
-            sports.forEach(sport => {
-                const option = document.createElement('option');
-                option.value = sport.sportId;
-                option.text = sport.sportName;
-                sportDropdown.appendChild(option);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching sports:', error);
-        });
-}
-
 function createTeam(event) {
     event.preventDefault();
 
@@ -103,23 +51,5 @@ function createTeam(event) {
             displayErrorMessage("Ocurrió un error al crear el equipo.");
         });
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('/api/googleMapsApiKey')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to fetch API key: ${response.status} ${response.statusText}`);
-            }
-            return response.text();
-        })
-        .then(apiKey => {
-            initAutocomplete(apiKey);
-        })
-        .catch(error => {
-            console.error('Error fetching API key:', error);
-        });
-
-    fetchSports();
-});
 
 
