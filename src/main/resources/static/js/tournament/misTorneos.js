cca8faace29cf8aaf7cb4611f61c77bb0595652f
husseymarcos <mhussey@mail.austral.edu.ar>
2024-12-10 18:09:35 -0300

    fixing show real location

--- src/main/resources/static/js/tournament/misTorneos.js
+++ src/main/resources/static/js/tournament/misTorneos.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchAndLoadGoogleMapsAPI()
+        .then(() => {
+        })
+        .catch(error => {
+            console.error('Error fetching API key:', error);
+        });
+});
+
 function cargarTorneos() {
     const userId = localStorage.getItem("userId");
     if (!userId) {
@@ -36,23 +45,39 @@ function cargarTorneos() {
                 const maxTeams = tournament.maxTeams;
                 const sportName = tournamentSport.sportName;
 
+                let tournamentAddress = tournamentLocation;
+                if (tournamentLocation) {
+                    const [lat, lng] = tournamentLocation.split(',').map(coord => parseFloat(coord.trim()));
+
+                    if (!isNaN(lat) && !isNaN(lng)) {
+                        reverseGeocode(lat, lng)
+                            .then(address => {
+                                tournamentAddress = address;
+                            })
+                            .catch(error => {
+                                tournamentAddress = error;
+                                console.error(error);
+                            });
+                    }
+                }
+
                 if (privateTournament && isCreator) {
                     fetch(`/api/requests/tournament/${userId}/${tournamentId}`)
                         .then(response => response.json())
                         .then(requests => {
                             const numOfRequests = requests.length;
                             li.innerHTML = `
-                            <div>
-                                <a href="loadTournament.html?id=${tournament.id}"><h3>${tournamentName}</h3></a> 
-                                <p>Deporte: ${sportName}</p>
-                                <p>Ubicación: ${tournamentLocation}</p>
-                                <p>Privacidad: ${privateTournament ? "Privado" : "Público"}</p>
-                                <p>Dificultad: ${tournament.difficulty}</p>
-                                <p>Equipos Participantes: ${numOfParticipatingTeams} / ${maxTeams}</p>
-                                ${isCreator ? `<a class="action-link" onclick="editarTorneo(${tournamentId})">Editar</a>
-                                <a class="action-link" onclick="borrarTorneo(${tournamentId})">Borrar</a>` : ''}
-                                ${privateTournament && isCreator ? `<a class="action-link" onclick="manejarSolicitudes(${tournamentId})">Manejar Solicitudes${numOfRequests > 0 ? ` <span class="request-count">(${numOfRequests})</span>` : ''}</a>` : ''}
-                            </div>
+                                <div>
+                                    <a href="loadTournament.html?id=${tournament.id}"><h3>${tournamentName}</h3></a> 
+                                    <p>Deporte: ${sportName}</p>
+                                    <p>Ubicación: ${tournamentAddress}</p>
+                                    <p>Privacidad: ${privateTournament ? "Privado" : "Público"}</p>
+                                    <p>Dificultad: ${tournament.difficulty}</p>
+                                    <p>Equipos Participantes: ${numOfParticipatingTeams} / ${maxTeams}</p>
+                                    ${isCreator ? `<a class="action-link" onclick="editarTorneo(${tournamentId})">Editar</a>
+                                    <a class="action-link" onclick="borrarTorneo(${tournamentId})">Borrar</a>` : ''}
+                                    ${privateTournament && isCreator ? `<a class="action-link" onclick="manejarSolicitudes(${tournamentId})">Manejar Solicitudes${numOfRequests > 0 ? ` <span class="request-count">(${numOfRequests})</span>` : ''}</a>` : ''}
+                                </div>
                             `;
                             if (isCreator) {
                                 listaTorneosCreados.appendChild(li);
@@ -63,16 +88,16 @@ function cargarTorneos() {
                         .catch(error => console.error('Error fetching tournament requests:', error));
                 } else {
                     li.innerHTML = `
-                    <div>
-                        <a href="loadTournament.html?id=${tournament.id}"><h3>${tournamentName}</h3></a> 
-                        <p>Deporte: ${sportName}</p>
-                        <p>Ubicación: ${tournamentLocation}</p>
-                        <p>Privacidad: ${privateTournament ? "Privado" : "Público"}</p>
-                        <p>Dificultad: ${tournament.difficulty}</p>
-                        <p>Equipos Participantes: ${numOfParticipatingTeams} / ${maxTeams}</p>
-                        ${isCreator ? `<a class="action-link" onclick="editarTorneo(${tournamentId})">Editar</a>
-                        <a class="action-link" onclick="borrarTorneo(${tournamentId})">Borrar</a>` : ''}
-                    </div>
+                        <div>
+                            <a href="loadTournament.html?id=${tournament.id}"><h3>${tournamentName}</h3></a> 
+                            <p>Deporte: ${sportName}</p>
+                            <p>Ubicación: ${tournamentAddress}</p>
+                            <p>Privacidad: ${privateTournament ? "Privado" : "Público"}</p>
+                            <p>Dificultad: ${tournament.difficulty}</p>
+                            <p>Equipos Participantes: ${numOfParticipatingTeams} / ${maxTeams}</p>
+                            ${isCreator ? `<a class="action-link" onclick="editarTorneo(${tournamentId})">Editar</a>
+                            <a class="action-link" onclick="borrarTorneo(${tournamentId})">Borrar</a>` : ''}
+                        </div>
                     `;
                     if (isCreator) {
                         listaTorneosCreados.appendChild(li);
